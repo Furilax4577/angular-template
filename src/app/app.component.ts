7fb34ee9261bb0948c6b5d2474304b688de93b47
Douville Quentin <douville.quentin@gmail.com>
2025-04-02 23:48:54 +0200

    Test SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { TrackingManagerService } from './services/tracking-manager.service';
 import { CookieBannerComponent } from './components/cookie-banner/cookie-banner.component';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,22 @@ import { CookieBannerComponent } from './components/cookie-banner/cookie-banner.
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor(private trackingManager: TrackingManagerService) {}
+  constructor(
+    private trackingManager: TrackingManagerService,
+    private meta: Meta,
+    private title: Title
+  ) {
+    this.title.setTitle('Angular project');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Angular project description',
+    });
+    this.meta.updateTag({
+      name: 'keywords',
+      content: 'angular, project',
+    });
+  }
+
   ngOnInit(): void {
     this.trackingManager.initIfConsented();
   }
